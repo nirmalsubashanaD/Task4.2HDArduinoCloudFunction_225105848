4784d6f09a5b0b44f8b456c81f21dcee62763203
nirmalsubashanaD <s225105848@deakin.edu.au>
2025-04-19 12:46:05 +1000

    Add files via upload

---
+++ Task5.3HD_225105848.ino
@@ -0,0 +1,56 @@
+#include <WiFiNINA.h>
+#include <ArduinoHttpClient.h>
+
+// network credentials
+char ssid[] = "SL_Kollo";
+char pass[] = "hondaaccord123@";
+
+// Firebase Realtime Database details
+const char* firebaseHost = "remotetrafficctrl-default-rtdb.firebaseio.com";
+const int httpsPort = 443;
+
+// Pins for LEDs
+const int redPin = 2;
+const int greenPin = 3;
+const int bluePin = 4;
+
+// Create WiFi and HTTP client
+WiFiSSLClient wifiClient;
+HttpClient client = HttpClient(wifiClient, firebaseHost, httpsPort);
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  // Connect to WiFi
+  Serial.print("Connecting to WiFi");
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("\nConnected!");
+}
+
+void loop() {
+  client.get("/leds.json"); // REST endpoint to get all LED states
+
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.println("Status code: " + String(statusCode));
+  Serial.println("Response: " + response);
+
+  // Parse simple response manually
+  bool redState = response.indexOf("\"red\":true") > 0;
+  bool greenState = response.indexOf("\"green\":true") > 0;
+  bool blueState = response.indexOf("\"blue\":true") > 0;
+
+  digitalWrite(redPin, redState ? HIGH : LOW);
+  digitalWrite(greenPin, greenState ? HIGH : LOW);
+  digitalWrite(bluePin, blueState ? HIGH : LOW);
+
+  delay(2000); // Poll every 2 seconds
+}
